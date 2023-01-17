0c38e79c83085a6ff0eb1b6fe557e9d9b23cac6f
UniqueUlysees <boriscool007@gmail.com>
2023-01-18 01:35:00 +0300

    Initial Code Load. Version 1.0.0

---
+++ README.md
@@ -0,0 +1,31 @@
+# gatsby-remark-directives
+
+Processs markdown files and adds commonmark styled directives 
+
+
+# Install
+
+    npm install gatsby-remark-directives
+
+# How to setup
+
+    {
+	    resolve: `gatsby-transformer-remark`,
+	    options: {
+		    plugins: [
+			    'gatsby-remark-directives'
+		    ]
+	    }
+    }
+
+# How to use 
+
+To be used with [gatsby-remark-component](https://www.gatsbyjs.com/plugins/gatsby-remark-component/). Thus look into that plugin.
+
+# Supported versions
+
+|                |ASCII                          
+|----------------|-------------------------------
+|Gatsby 5        |`Unknown`            
+|Gatsby 4        |`Supported`            
+|Gatsby 3        |`Unknown`
---
+++ index.js
@@ -0,0 +1,23 @@
+const esmRequire = require('./esmRequire')
+
+const remarkDirective = esmRequire("remark-directive").default
+const visit = esmRequire('unist-util-visit').visit;
+const h = esmRequire('hastscript').h;
+
+module.exports.setParserPlugins = () => [remarkDirective] 
+module.exports.default = ({ markdownAST }, pluginOptions) => {
+    visit(markdownAST, (node) => {
+        if (
+        node.type === 'textDirective' ||
+        node.type === 'leafDirective' ||
+        node.type === 'containerDirective'
+        ) {
+        const data = node.data || (node.data = {})
+        const hast = h(node.name, node.attributes);
+
+        data.hName = hast.tagName;
+        data.hProperties = hast.properties;
+        }
+    })
+  }
+    
