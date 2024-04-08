2217d6abfd3b5f0391b785ea896722566b9a510d
Yuriy Baranov <you@example.com>
2024-04-08 19:36:55 +0530

    Plugin refactoring
    - dependencies list update

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-const esmRequire = require('./esmRequire')
+const esmRequire = require("./esmRequire");
 
-const remarkDirective = esmRequire("remark-directive").default
-const visit = esmRequire('unist-util-visit').visit;
-const h = esmRequire('hastscript').h;
+const remarkDirective = esmRequire("remark-directive");
+const { visit } = esmRequire("unist-util-visit");
+const { h } = esmRequire("hastscript");
 
-module.exports.setParserPlugins = () => [remarkDirective] 
+module.exports.setParserPlugins = () => [remarkDirective];
 module.exports.default = ({ markdownAST }, pluginOptions) => {
-    visit(markdownAST, (node) => {
-        if (
-        node.type === 'textDirective' ||
-        node.type === 'leafDirective' ||
-        node.type === 'containerDirective'
-        ) {
-        const data = node.data || (node.data = {})
-        const hast = h(node.name, node.attributes);
+  visit(markdownAST, (node) => {
+    if (
+      node.type === "textDirective" ||
+      node.type === "leafDirective" ||
+      node.type === "containerDirective"
+    ) {
+      const data = node.data || (node.data = {});
+      const hast = h(node.name, node.attributes);
 
-        data.hName = hast.tagName;
-        data.hProperties = hast.properties;
-        }
-    })
-  }
-    
+      data.hName = hast.tagName;
+      data.hProperties = hast.properties;
+    }
+  });
+};
